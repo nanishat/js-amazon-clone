2d4ac13b315c710d37ced58f2eed3743212449c4
nanishat <spy.spid3y@gmail.com>
2025-08-22 22:21:55 +0600

    made the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -83,6 +83,15 @@ document.querySelectorAll('.js-add-to-cart')
         });
       }
 
-      console.log(cart);
+      //to change total cart at the top right corner
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+
     });
   });
