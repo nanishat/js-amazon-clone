c306720f3e50072aae88030065684b63499c0aed
nanishat <spy.spid3y@gmail.com>
2025-08-26 23:45:37 +0600

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,19 @@
-export let cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
-}, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+    cart = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2
+    }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+    }];
+}
+
+//save cart to the localStorage
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 // Function: update cart based on productId and dropdown
 export function updateCart(productId) {
@@ -34,6 +43,8 @@ export function updateCart(productId) {
             quantity
         });
     }
+
+    saveToStorage();
 }
 
 //an empty object to store each products timeout
@@ -61,6 +72,7 @@ export function addedTextFeature(productId) {
     //console.log(addedMessageTimeouts); //you can observe it
 }
 
+//Function: remove from the cart based on productId
 export function removeFromCart(productId) {
     const newCart = [];
 
@@ -71,4 +83,6 @@ export function removeFromCart(productId) {
     });
 
     cart = newCart;
+
+    saveToStorage();
 }
