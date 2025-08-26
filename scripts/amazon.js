f5439885ff5338510ca32535c1e1252498a4f750
nanishat <spy.spid3y@gmail.com>
2025-08-26 11:08:01 +0600

    moved updateCart & addedTextFeature function as a module into cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from '../data/cart.js';
+import { cart, updateCart, addedTextFeature } from '../data/cart.js';
 import { products } from '../data/products.js';
 
 let productsHTML = '';
@@ -76,37 +76,6 @@ document.querySelectorAll('.js-add-to-cart')
     });
   });
 
-
-// Function: update cart based on productId and dropdown
-function updateCart(productId) {
-  const dropDown = document.querySelector(`.js-quantity-selector-${productId}`);
-  const quantity = Number(dropDown.value);
-
-  let matchingItem;
-
-  cart.forEach((cartItem) => {
-    //cartItem = productId and quantity; as we pushed it
-    if (productId === cartItem.productId) {
-      //if product has found in the cartItem, we can store that product into a variable
-      matchingItem = cartItem;
-    }
-  });
-
-  //if we did find a productID/productId in the cartItem, it will be stored in the matchingItem, eventually it will return truthy value
-  if (matchingItem) {
-    matchingItem.quantity += quantity;
-  } else {
-    cart.push({
-      //productId: productId,
-      //quantity: quantity
-
-      //using shorthand
-      productId,
-      quantity
-    });
-  }
-}
-
 // Function: update cart quantity on UI
 function updateCartQuantityUI() {
   let cartQuantity = 0;
@@ -117,29 +86,4 @@ function updateCartQuantityUI() {
 
   document.querySelector('.js-cart-quantity')
     .innerHTML = cartQuantity;
-}
-
-//an empty object to store each products timeout
-const addedMessageTimeouts = {};
-
-// Function: show 'Added' message based on productId
-function addedTextFeature(productId) {
-
-  const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
-  addedMessage.classList.add('added-to-cart-visible');
-
-  //check, if there's a previous timeout for this product, If there is, we should stop it
-  const previousTimeoutID = addedMessageTimeouts[productId];
-
-  if (previousTimeoutID) {
-    clearTimeout(previousTimeoutID);
-  }
-
-  const timeoutID = setTimeout(() => {
-    addedMessage.classList.remove('added-to-cart-visible');
-  }, 2000);
-
-  //saving the timeoutID for this prodcut, so we can stop it later if we need to...
-  addedMessageTimeouts[productId] = timeoutID;
-  //console.log(addedMessageTimeouts); //you can observe it
 }
