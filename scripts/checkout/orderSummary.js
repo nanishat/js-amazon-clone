c8c1623f8202225d81b00fb969e9f091b29a00d4
nanishat <spy.spid3y@gmail.com>
2025-09-09 21:07:04 +0600

    created automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -24,6 +24,7 @@ export function renderOrderSummary() {
 
     cartSummaryHTML += `
     <div class="cart-item-container 
+        js-cart-item-container 
         js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
         Delivery date: ${dateString}
@@ -39,7 +40,8 @@ export function renderOrderSummary() {
             <div class="product-price">
             $${formatCurrency(matchingProduct.priceCents)}
             </div>
-            <div class="product-quantity">
+            <div class="product-quantity
+              js-product-quantity-${matchingProduct.id}">
             <span>
                 Quantity: <span class="quantity-label js-quantity-label${matchingProduct.id}">${cartItem.quantity}</span>
             </span>
@@ -48,8 +50,9 @@ export function renderOrderSummary() {
             <input class="quantity-input js-quantity-input-${matchingProduct.id}">
             <span class="save-quantity-link link-primary js-save-link" 
             data-product-id="${matchingProduct.id}">Save</span>
-            <span class="delete-quantity-link link-primary js-delete-link"
-            data-product-id="${matchingProduct.id}">Delete</span>
+            <span class="delete-quantity-link link-primary js-delete-link 
+              js-delete-link-${matchingProduct.id}"
+              data-product-id="${matchingProduct.id}">Delete</span>
             </div>
         </div>
 
---
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -0,0 +1,79 @@
+import { renderOrderSummary } from '../../scripts/checkout/orderSummary.js';
+import { loadFromStorage, cart } from "../../data/cart.js";
+
+//integration test
+describe('test suite: renderOrderSummary', () => {
+
+  //for access globally
+  const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+  const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+  //beforeEach hooks
+  beforeEach(() => {
+    document.querySelector('.js-test-container').innerHTML = `
+      <div class="js-order-summary"></div>
+      <div class="js-return-to-home-link"></div>
+      <div class="js-payment-summary"></div>
+    `;
+
+    //in order to prevent saving mock into localStorage
+    spyOn(localStorage, 'setItem');
+
+    //mock: localStorage
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: productId1,
+        quantity: 2,
+        deliveryOptionId: '1'
+      }, {
+        productId: productId2,
+        quantity: 1,
+        deliveryOptionId: '2'
+      }]);
+    });
+    loadFromStorage();
+
+    renderOrderSummary();
+  });
+
+  //afterEach hooks
+  afterEach(() => {
+    document.querySelector('.js-test-container').innerHTML = '';
+  });
+
+  //how the page looks
+  it('displays the cart', () => {
+    expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(2);
+    expect(
+      document.querySelector(`.js-product-quantity-${productId1}`).innerText
+    ).toContain('Quantity: 2');
+    expect(
+      document.querySelector(`.js-product-quantity-${productId2}`).innerText
+    ).toContain('Quantity: 1');
+  });
+
+  //how the page behaves
+  it('removes a product', () => {
+    document.querySelector(`.js-delete-link-${productId1}`).click();
+    expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(1);
+    expect(
+      document.querySelector(`.js-cart-item-container-${productId1}`)
+    ).toEqual(null);
+    expect(
+      document.querySelector(`.js-cart-item-container-${productId2}`)
+    ).not.toEqual(null);
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual(productId2);
+  });
+});
+
+/*
+unit tests: testing one piece of code
+integration test: tests many units/pieces of code working together
+
+Hooks in Jasmine:
+beforeEach() = runs code before each test
+afterEach() = runs code after each test
+beforeAll() = runs code before all test
+afterAll() = runs code after all test
+*/
