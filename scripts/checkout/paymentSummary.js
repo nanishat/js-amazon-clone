286b26335115ff09ef295ab1936c1c4ddada854b
nanishat <spy.spid3y@gmail.com>
2025-09-10 02:16:32 +0600

    added a test case into orderSummaryTest.js

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -40,7 +40,8 @@ export function renderPaymentSummary() {
 
       <div class="payment-summary-row">
         <div>Shipping &amp; handling:</div>
-        <div class="payment-summary-money">
+        <div class="payment-summary-money
+          js-payment-summary-shipping">
           $${formatCurrency(shippingPriceCents)}
         </div>
       </div>
@@ -61,7 +62,8 @@ export function renderPaymentSummary() {
 
       <div class="payment-summary-row total-row">
         <div>Order total:</div>
-        <div class="payment-summary-money">
+        <div class="payment-summary-money
+          js-payment-summary-total">
           $${formatCurrency(totalCents)}
         </div>
       </div>
--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -69,6 +69,7 @@ describe('test suite: renderOrderSummary', () => {
   //how the page behaves
   it('removes a product', () => {
     document.querySelector(`.js-delete-link-${productId1}`).click();
+
     expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(1);
     expect(
       document.querySelector(`.js-cart-item-container-${productId1}`)
@@ -85,7 +86,27 @@ describe('test suite: renderOrderSummary', () => {
       document.querySelector(`.js-product-price-${productId2}`).innerText
     ).toEqual('$20.95');
   });
+
+  //how the page behave
+  it('updates the delivery option', () => {
+    document.querySelector(`.js-delivery-option-${productId1}-3`).click();
+
+    expect(
+      document.querySelector(`.js-delivery-option-input-${productId1}-3`).checked
+    ).toEqual(true);
+    expect(cart.length).toEqual(2);
+    expect(cart[0].deliveryOptionId).toEqual('3');
+
+    expect(
+      document.querySelector('.js-payment-summary-shipping').innerText
+    ).toEqual('$14.98');
+    expect(
+      document.querySelector('.js-payment-summary-total').innerText
+    ).toEqual('$63.50');
+  });
 });
+
+
 
 /*
 unit tests: testing one piece of code
