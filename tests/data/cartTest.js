c76b392cf85dd4ec4713fe359b494745904f07a5
nanishat <spy.spid3y@gmail.com>
2025-09-10 01:10:54 +0600

    added a test suite into cartTest.js

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import { updateCart, cart, loadFromStorage } from "../../data/cart.js";
+import { updateCart, cart, loadFromStorage, removeFromCart } from "../../data/cart.js";
 
 describe('test suite: updateCart', () => {
   //beforeEach hooks
@@ -52,8 +52,45 @@ describe('test suite: updateCart', () => {
   });
 });
 
+describe('test suite: removeFromCart', () => {
+  //beforeEach hooks
+  beforeEach(() => {
+    //in order to prevent saving mock into localStorage
+    spyOn(localStorage, 'setItem');
+
+    //mock: localStorage
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
+    });
+    loadFromStorage();
+  });
+
+  it('removes a product from the cart', () => {
+    removeFromCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart.length).toEqual(0);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([]));
+  });
+
+  it('does nothing if product is not in the cart', () => {
+    removeFromCart('does-not-exist');
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 1,
+      deliveryOptionId: '1'
+    }]));
+  });
+});
+
 /*
-test case 2:
 flaky test = test that sometimes passes and sometimes fails
 mock = lets us replace a method with a fake version
 to execute mock: spyOn(object, 'method name') --- which returns an object, there is a property in that object named 'and', which we can use to call another method named 'callFake' AKA Mock...
