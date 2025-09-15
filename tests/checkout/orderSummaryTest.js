51bfab91145c7949cd65269b391717e51814ee4e
nanishat <spy.spid3y@gmail.com>
2025-09-15 19:53:58 +0600

    load products in beforeAll hook

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from '../../scripts/checkout/orderSummary.js';
 import { cart } from "../../data/cart-class.js";
+import { loadProducts } from '../../data/products.js';
 
 //integration test
 describe('test suite: renderOrderSummary', () => {
@@ -9,6 +10,13 @@ describe('test suite: renderOrderSummary', () => {
   const productName1 = 'Black and Gray Athletic Cotton Socks - 6 Pairs';
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
   const productName2 = 'Intermediate Size Basketball';
+
+  //beforeAll hooks
+  beforeAll((done) => {
+    loadProducts(() => {
+      done();
+    });
+  });
 
   //beforeEach hooks
   beforeEach(() => {
