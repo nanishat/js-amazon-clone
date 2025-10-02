889de62f30e5310e39ffdfc2a87c682a08102b72
nanishat <spy.spid3y@gmail.com>
2025-10-02 21:19:34 +0600

    made the tracking page interactive

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -22,13 +22,20 @@ async function loadPage() {
     }
   })
 
+  const today = dayjs();
+  const orderTime = dayjs(order.orderTime);
+  const deliveryTime = dayjs(
+    productDetails.estimatedDeliveryTime
+  );
+  const percentProgress = ((today - orderTime) / (deliveryTime - orderTime)) * 100;
+
   trackingHTML += `
   <a class="back-to-orders-link link-primary" href="orders.html">
     View all orders
   </a>
 
   <div class="delivery-date">
-    Arriving on ${dayjs(productDetails.estimatedDeliveryTime).format('dddd, MMMM d')}
+    Arriving on ${deliveryTime.format('dddd, MMMM d')}
   </div>
 
   <div class="product-info">
@@ -42,19 +49,25 @@ async function loadPage() {
   <img class="product-image" src="${product.image}">
 
   <div class="progress-labels-container">
-    <div class="progress-label">
+    <div class="progress-label
+      ${percentProgress < 50 ? 'current-status' : ''}
+    ">
       Preparing
     </div>
-    <div class="progress-label current-status">
+    <div class="progress-label
+    ${percentProgress >= 50 && percentProgress < 100 ? 'current-status' : ''}
+    ">
       Shipped
     </div>
-    <div class="progress-label">
+    <div class="progress-label
+    ${percentProgress >= 100 ? 'current-status' : ''}
+    ">
       Delivered
     </div>
   </div>
 
   <div class="progress-bar-container">
-    <div class="progress-bar"></div>
+    <div class="progress-bar" style="width: ${percentProgress}%;"></div>
   </div>
 `;
   document.querySelector('.js-order-tracking')
